56704b1403024411bfdd6092c9d7c06b4c630e4b
florans1807 <florans1807@MAIL.RU>
2021-01-12 12:29:45 +0300

    commit 1

--- 2.2.2. spring_mvc/src/main/java/web/controller/UserController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.service.UserServiceSF;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- 2.2.2. spring_mvc/src/main/java/web/service/UserService0.java
+++
@@ -1,9 +0,0 @@
-package web.service;
-
-import web.model.User;
-
-import java.util.List;
-
-public interface UserService0 {
-    List<User> getAll();
-}
