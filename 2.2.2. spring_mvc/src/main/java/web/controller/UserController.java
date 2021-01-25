49adff993cfd6aa573ee7abc7bef8aa13d3bf0c7
iakov <iakovlevjenya21@mail.ru>
2021-01-25 21:46:35 +0300

    commit 1

--- 2.2.2. spring_mvc/src/main/java/web/controller/UserController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/UserController.java
@@ -3,14 +3,17 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserServiceIn;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/")
 public class UserController {
 
     private final UserServiceIn userService;
@@ -20,19 +23,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping()
+    @GetMapping("/users")
     public String index(Model model) {
         model.addAttribute("users", userService.getAll());
         return "obj";
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/users/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("user", userService.get(id));
         return "show";
     }
 
-    @GetMapping("/new")
+    @GetMapping("/users/new")
     public String newPerson(@ModelAttribute("user") User user) {
         return "new";
     }
@@ -43,22 +46,37 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @GetMapping("/{id}/edit")
+    @GetMapping("/users/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("user", userService.get(id));
         return "edit";
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("/users/{id}")
     public String update(@ModelAttribute("user") User user) {
         userService.update(user);
         return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/users/{id}")
     public String delete(@PathVariable("id") int id) {
         userService.delete(id);
         return "redirect:/users";
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC-SECURITY application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String loginPage() {
+        return "login";
     }
 
 }
--- 2.2.2. spring_mvc/src/main/java/web/model/User.java
+++ 2.2.2. spring_mvc/src/main/java/web/model/User.java
@@ -1,10 +1,15 @@
 package web.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @Column(name = "Id")
@@ -14,16 +19,13 @@ public class User {
     @Column(name = "firstname")
     private String name;
 
-    @Column(name = "secondname")
-    private String surname;
+    @Column(name= "pass")
+    private String password;
+
+    @Column(name = "Role")
+    private Set<Role> roles;
 
     public User() {};
-
-    public User(int id, String name, String surname) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-    }
 
     public User(int id, String name) {
         this.id = id;
@@ -46,11 +48,52 @@ public class User {
         this.name = name;
     }
 
-    public String getSurname() {
-        return surname;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
